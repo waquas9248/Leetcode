141381c4e19779a74e75e49e5ad9199c7b4f7d84
waquas9248 <55626672+waquas9248@users.noreply.github.com>
2025-04-06 18:28:43 -0500

    Create HouseRobber.java

---
+++ HouseRobber.java
@@ -0,0 +1,26 @@
+// You are given an integer array nums where nums[i] represents the amount of money the ith house has. The houses are arranged in a straight line, i.e. the ith house is the neighbor of the (i-1)th and (i+1)th house.
+// You are planning to rob money from the houses, but you cannot rob two adjacent houses because the security system will automatically alert the police if two adjacent houses were both broken into.
+// Return the maximum amount of money you can rob without alerting the police.
+
+class Solution {
+    public int rob(int[] nums) {
+
+        if( nums.length == 1 )
+            return nums[0];
+        
+        if( nums.length == 2 )
+            return Math.max(nums[0], nums[1]);
+
+        int[] money = new int[nums.length];
+
+        money[0] = nums[0];
+        money[1] = nums[1];
+
+        for(int i = 2 ; i < nums.length ; i++)
+            //Max of total until 2 houses back or total until 3 houses back
+            money[i] = nums[i] + Math.max(money[i-2], money[i-1] - nums[i-1] );
+
+        return Math.max( money[nums.length - 2], money[nums.length - 1] );
+      
+    }
+}
